99970f8284c155caace0d98d1da93f8046d22096
Ilê Caian <ile.caian@gmail.com>
2021-08-20 13:01:44 -0300

    refactor: use functional component with useMemo

--- src/LinearGradient.js
+++ src/LinearGradient.js
@@ -1,41 +1,37 @@
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import React, { useMemo } from 'react'
 import Svg, { Defs, LinearGradient as SVGLinearGradient, Rect, Stop } from 'react-native-svg'
 import { getAnglePercentageObject } from './utils'
 
-class LinearGradient extends Component {
+function LinearGradient({ angle, colorList }) {
+  const { x1, x2, y1, y2 } = useMemo(() => getAnglePercentageObject(angle), [angle])
 
-  render () {
-    const { angle, colorList } = this.props
-    const angleObj = getAnglePercentageObject(angle)
-
-    return (
-      <Svg height="100%" width="100%">
-        <Defs>
-          <SVGLinearGradient
-            id="grad"
-            x1={`${angleObj.x1}%`}
-            y1={`${angleObj.y1}%`}
-            x2={`${angleObj.x2}%`}
-            y2={`${angleObj.y2}%`}
-          >
-            {colorList.map((value, index) => <Stop
-              key={`LinerGradientItem_${index}`}
-              offset={value.offset}
-              stopColor={value.color}
-              stopOpacity={value.opacity}/>)}
-          </SVGLinearGradient>
-        </Defs>
-        <Rect
-          x="0"
-          y="0"
-          width="100%"
-          height="100%"
-          fill="url(#grad)"
-        />
-      </Svg>
-    )
-  }
+  return (
+    <Svg height="100%" width="100%">
+      <Defs>
+        <SVGLinearGradient
+          id="grad"
+          x1={`${x1}%`}
+          y1={`${y1}%`}
+          x2={`${x2}%`}
+          y2={`${y2}%`}
+        >
+          {colorList.map((value, index) => <Stop
+            key={`LinerGradientItem_${index}`}
+            offset={value.offset}
+            stopColor={value.color}
+            stopOpacity={value.opacity}/>)}
+        </SVGLinearGradient>
+      </Defs>
+      <Rect
+        x="0"
+        y="0"
+        width="100%"
+        height="100%"
+        fill="url(#grad)"
+      />
+    </Svg>
+  )
 }
 
 LinearGradient.propTypes = {
